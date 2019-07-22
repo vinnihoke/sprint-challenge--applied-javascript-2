709fa81cb2473510b88eb3b5c769d9ce63e9ca6e
Vinni Hoke <vinni.hoke@gmail.com>
2019-07-21 21:27:06 -0700

    Partially Completed Stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -9,11 +9,67 @@
 
 /* HTML:
   <div class="carousel">
-    <div class="left-button"> < </div>
+    <div class="left-button"> 
+      <i class="material-icons">keyboard_arrow_left</i>
+    </div>
     <img src="./assets/carousel/mountains.jpeg" />
     <img src="./assets/carousel/computer.jpeg" />
     <img src="./assets/carousel/trees.jpeg" />
     <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
+    <div class="right-button"> 
+      <i class="material-icons">keyboard_arrow_right</i> 
+    </div>
   </div>
 */
+
+const Carousel = () => {
+  const mount = document.querySelector('.carousel-container');
+
+  // Create div.carousel
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+
+  // Create div.left-button, append to carousel. "<" or Icon.
+  const leftButton = document.createElement('div');
+  const leftButtonIcon = document.createElement('i');
+  leftButton.classList.add('left-button');
+  leftButtonIcon.classList.add('material-icons');
+  leftButtonIcon.textContent = 'keyboard_arrow_left';
+  leftButton.appendChild(leftButtonIcon);
+  carousel.appendChild(leftButton);
+
+  // Create img src="./assets/carousel/mountains.jpeg", append to carousel.
+  const img1 = document.createElement('img');
+  img1.src = './assets/carousel/mountains.jpeg';
+  carousel.appendChild(img1);
+  console.log(img1);
+
+  // Create img src="./assets/carousel/computer.jpeg", append to carousel.
+  const img2 = document.createElement('img');
+  img2.src = './assets/carousel/computer.jpeg';
+  carousel.appendChild(img2);
+
+  // Create img src="./assets/carousel/trees.jpeg", append to carousel.
+  const img3 = document.createElement('img');
+  img3.src = './assets/carousel/trees.jpeg';
+  
+  carousel.appendChild(img3);
+
+  // Create img src="./assets/carousel/turntable.jpeg", append to carousel.
+  const img4 = document.createElement('img');
+  img4.src = './assets/carousel/turntable.jpeg';
+  carousel.appendChild(img4);
+
+  // Create div.right-button, append to carousel. ">" or Icon.
+  const rightButton = document.createElement('div');
+  const rightButtonIcon = document.createElement('i');
+  rightButton.classList.add('right-button');
+  rightButtonIcon.classList.add('material-icons');
+  rightButtonIcon.textContent = 'keyboard_arrow_right';
+  rightButton.appendChild(rightButtonIcon);
+  carousel.appendChild(rightButton);
+
+  mount.appendChild(carousel);
+}
+
+Carousel();
